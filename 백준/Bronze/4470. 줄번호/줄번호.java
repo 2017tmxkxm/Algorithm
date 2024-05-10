91bcc7cba2a6c033d435fe3434e37fab26f4de7a
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-05-10 16:24:15 +0900

    [Bronze IV] Title: 줄번호, Time: 152 ms, Memory: 16160 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<N+1; i++) {
+            String a = br.readLine();
+            System.out.println(i + ". " + a);
+        }
+    }
+}
