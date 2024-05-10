d725edb87627fdf7166418505bc1efbe8987d379
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-05-10 16:18:55 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min = Math.min(s, Math.min(k, h));
+        if (s + k + h >= 100) {
+            System.out.println("OK");
+        }else {
+            if (min == s) {
+                System.out.println("Soongsil");
+            } else if (min == k) {
+                System.out.println("Korea");
+            } else {
+                System.out.println("Hanyang");
+            }
+        }
+    }
+}
