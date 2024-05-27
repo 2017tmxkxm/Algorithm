da38a2d9ac92522f62c78f7e2737eb2871244873
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-05-27 15:48:11 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            System.out.println(" ".repeat(n-i-1) + "*".repeat(2*i+1));
+        }
+    }
+}
