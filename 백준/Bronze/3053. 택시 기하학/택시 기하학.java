645e9c40db1cb2387e8c7128ad1ec0d444c601c3
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-06-05 11:24:55 +0900

    [Bronze III] Title: 택시 기하학, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextInt();
+
+        System.out.println(Math.PI*n*n);
+        System.out.println(2*n*n);
+
+    }
+}
