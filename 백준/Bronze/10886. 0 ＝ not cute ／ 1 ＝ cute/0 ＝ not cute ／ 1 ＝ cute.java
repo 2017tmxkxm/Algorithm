4ab6d88c84bce84097ebdccfaa515e70461145d2
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-06-12 22:44:48 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 212 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+        for(int i=0; i<N; i++) {
+            int temp = sc.nextInt();
+            if(temp ==1) count++;
+        }
+        
+        if(count>N/2) {
+            System.out.println("Junhee is cute!");
+        } else {
+            System.out.println("Junhee is not cute!");
+        }
+    }
+}
