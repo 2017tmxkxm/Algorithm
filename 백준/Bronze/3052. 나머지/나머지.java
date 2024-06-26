f665ea5b672ac3fe7a21d4be8ab6297cc7b32c8f
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-06-26 16:53:38 +0900

    [Bronze II] Title: 나머지, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,23 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] num = new int[10];
+        Set<Integer> num = new HashSet<>();
         for(int i=0; i<10; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            num[i] =  a % 42;
+            int temp = Integer.parseInt(st.nextToken()) % 42;
+            num.add(temp);
         }
 
-        num = Arrays.stream(num).distinct().toArray();
-
-        System.out.println(num.length);
-
+        System.out.println(num.size());
     }
 }
