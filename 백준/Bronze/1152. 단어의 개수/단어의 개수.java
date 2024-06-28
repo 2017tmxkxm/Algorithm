4af122cfe245a726275e88aa4a6a7638fe1a7589
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-06-28 15:39:28 +0900

    [Bronze II] Title: 단어의 개수, Time: 296 ms, Memory: 26900 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int answer = 0;
+        for(int i=0; i<str.length; i++) {
+            if(!str[i].isEmpty()) {
+                answer ++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
