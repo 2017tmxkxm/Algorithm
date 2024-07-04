1fec59e47eee82e576c25c3284e1f3e73da56e1b
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-07-04 11:47:46 +0900

    [Silver II] Title: N과 M (11), Time: 2744 ms, Memory: 155464 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] num;
+    static int[] arr;
+    static LinkedHashSet<String> set = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        num = new int[N];
+        arr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        dfs(0);
+        set.forEach(System.out::println);
+    }
+
+    private static void dfs(int depth){
+        StringBuilder sb = new StringBuilder();
+        if(depth == M) {
+            for(int val : arr) {
+                sb.append(val).append(" ");
+            }
+            set.add(sb.toString());
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            arr[depth] = num[i];
+            dfs(depth+1);
+        }
+    }
+}
