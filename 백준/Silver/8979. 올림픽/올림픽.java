142be9fac53bac94d9438721e49a1651b8831f55
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2025-07-31 16:11:57 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14752 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -15,11 +15,11 @@ public class Main {
         ArrayList<Score_8979> arrayList = new ArrayList<>();
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            arrayList.add(new Score_8979(a, 100*b + 10*c + d));
+            int index = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+            arrayList.add(new Score_8979(index, gold, silver, bronze, 0));
         }
 
         Collections.sort(arrayList);
@@ -28,15 +28,18 @@ public class Main {
         int end_point = 0;
 
         for(int i=1; i<arrayList.size(); i++) {
-            int temp_sum = arrayList.get(i-1).sum;
+            int tmp_gold = arrayList.get(i-1).gold;
+            int tmp_silver  = arrayList.get(i-1).silver;
+            int tmp_bronze  = arrayList.get(i-1).bronze;
+            Score_8979 now = arrayList.get(i);
 
             if(arrayList.get(i).index == K) {
                 end_point = i;
             }
 
-            if(temp_sum == arrayList.get(i).sum) {
+            if (now.gold == tmp_gold && now.silver==tmp_silver && now.bronze==tmp_bronze) {
                 arrayList.get(i).rank = arrayList.get(i-1).rank;
-            }else {
+            } else {
                 arrayList.get(i).rank = i+1;
             }
         }
@@ -47,16 +50,32 @@ public class Main {
 
 class Score_8979 implements Comparable<Score_8979> {
     int index;
-    int sum;
+    int gold;
+    int silver;
+    int bronze;
     int rank;
 
-    public Score_8979(int index, int sum) {
+    public Score_8979(int index, int gold, int siver, int bronze, int rank) {
         this.index = index;
-        this.sum = sum;
+        this.gold = gold;
+        this.silver = siver;
+        this.bronze = bronze;
+        this.rank = rank;
     }
+
 
     @Override
     public int compareTo(Score_8979 o) {
-        return o.sum - this.sum;
+        if(this.gold==o.gold) {
+            if(this.silver==o.silver) {
+                return o.bronze-this.bronze;
+            }
+            else {
+                return o.silver-this.silver;
+            }
+        }
+        else {
+            return o.gold-this.gold;
+        }
     }
 }
