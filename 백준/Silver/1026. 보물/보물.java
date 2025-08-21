0112dcf3604d68757ec803c38efeec7e029dfcfa
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2025-08-21 11:14:22 +0900

    [Silver IV] Title: 보물, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,29 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        Integer[] A = new Integer[N];
-        Integer[] B = new Integer[N];
+        int[] A = new int[N];
+        int[] B = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
+        st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
-            B[i] = sc.nextInt();
+            B[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(A);
-        Arrays.sort(B, Collections.reverseOrder());
+        Arrays.sort(B);
 
         int sum = 0;
         for(int i=0; i<N; i++) {
-            sum += A[i] * B[i];
+            sum += A[i] * B[N-1-i];
         }
 
         System.out.println(sum);
