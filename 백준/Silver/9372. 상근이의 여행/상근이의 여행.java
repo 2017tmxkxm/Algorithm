901a1e3aaf639331ac4659a6ba58121d297e1576
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2025-09-17 17:06:44 +0900

    [Silver IV] Title: 상근이의 여행, Time: 956 ms, Memory: 242004 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            for(int j=0; j<M; j++) {
+                int s = sc.nextInt();
+                int e = sc.nextInt();
+            }
+            System.out.println(N-1);
+        }
+    }
+}
